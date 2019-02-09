37da7d7bd6f22ecc300287219add91017c288656
Kumudu Gunasekara <kumudug1@gmail.com>
2019-02-09 14:00:01 -0800

    using createElement

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>First react app.</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>First react app.</h1>
+    //   </div>
+    // );
+    let h1Element = React.createElement("h1", null, "First react app.");
+    return React.createElement("div", {className: 'App'}, h1Element);
   }
 }
 
