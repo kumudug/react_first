8e671723119f0c38f3045a6b9c2f09edb994a0ca
Kumudu Gunasekara <kumudug1@gmail.com>
2019-02-09 17:00:14 -0800

    State management

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Adams", age: 29 },
+      { name: "Baker", age: 27 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Max-Changed", age: 28 },
+        { name: "Adams", age: 29 },
+        { name: "Baker", age: 27 }
+      ]
+    });
+  };
+
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>First react app.</h1>
-    //   </div>
-    // );
-    let h1Element = React.createElement("h1", null, "First react app.");
-    return React.createElement("div", {className: 'App'}, h1Element);
+    return (
+      <div className="App">
+        <h1>First react app.</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          Hobbies: Sailing
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // let h1Element = React.createElement("h1", null, "First react app.");
+    // return React.createElement("div", {className: 'App'}, h1Element);
   }
 }
 
---
+++ hello_world/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm a {props.name} and I am {Math.floor(Math.random() * 30)} years old.
+        I look {props.age} years old. :D
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default person;
