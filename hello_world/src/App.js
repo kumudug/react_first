cd34aa4a95a0da5e0e29346d254ecf773fdc6162
Kumudu Gunasekara <kumudug1@gmail.com>
2019-02-10 16:37:23 -0800

    sudo styles using radium

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -47,7 +48,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     //let classes = ['red', 'bold'].join(' '); //what we get 'red bold'
@@ -95,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
