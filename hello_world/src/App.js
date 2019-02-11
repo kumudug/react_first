739292b23603bdab67f9181f1a3a143b6cb05fa2
Kumudu Gunasekara <kumudug1@gmail.com>
2019-02-10 16:28:12 -0800

    dymanic styling

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -42,7 +42,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: 'white',
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -69,11 +70,22 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    //let classes = ['red', 'bold'].join(' '); //what we get 'red bold'
+    const classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>First react app.</h1>
+        <p className={classes.join(' ')}>Dymanic Styling</p>
         <button onClick={this.togglPersonsHandler} style={style}>Toggle Persons</button>
         {persons}
       </div>
