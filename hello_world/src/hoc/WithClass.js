7df2203e8df1dca2f67327e1e3a38f0eb732fd89
kumudug <kumudug1@gmail.com>
2019-02-24 15:23:00 -0800

    another way to write higher order components

--- hello_world/src/hoc/WithClass.js
+++ hello_world/src/hoc/WithClass.js
@@ -5,5 +5,14 @@ const withClass = props => (
         {props.children}
     </div>
 );
+//Another way of doing the above
+// const withClass = (WrappedComponent, className) => {
+//     return props => (
+//         <div className={className}>
+//             <WrappedComponent />
+//         </div>
+//     );
+// };
+//use it like this in App.js withClass(App, classes.App);
 
 export default withClass;
