121f60cc7f1ce6286ddc5c9bb0e7f3497e226b83
kumudug <kumudug1@gmail.com>
2019-02-24 11:46:09 -0800

    Component creation lifecycle

--- hello_world/src/Components/Persons/Persons.js
+++ hello_world/src/Components/Persons/Persons.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return (
-        <Person
-            name={person.name}
-            age={person.age}
-            click={() => props.clicked(index)}
-            changed={event => this.changed(event, person.id)}>
-            Hobbies: sailing
+const persons = (props) => {
+    console.log('[Persons.js] rendering...');
+    return props.persons.map((person, index) => {
+        return (
+            <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={() => props.clicked(index)}
+                changed={event => this.changed(event, person.id)}>
+                Hobbies: sailing
         </Person>
-    );
-});
+        );
+    })
+};
 
 export default persons;
--- hello_world/src/containers/App.js
+++ hello_world/src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from "../Components/Persons/Persons";
 import Cockpit from "../Components/Cockpit/Cockpit";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: "01", name: "Max", age: 28 },
@@ -11,6 +17,15 @@ class App extends Component {
       { id: "03", name: "Baker", age: 27 }
     ]
   };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
 
   deletePersonHandler = personIndex => {
     //const persons = this.state.persons.slice();
@@ -42,6 +57,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
